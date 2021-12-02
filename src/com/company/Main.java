60be57852b7a3195fd32ba32b0e49ae97841ee26
BuFu9 <mads02p8@stud.kea.dk>
2021-12-02 14:55:16 +0100

    Fixet layout og switch case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,43 +9,64 @@ public class Main {
     private static SwimmerStats myStats = new SwimmerStats();
     private static Payment myPayments = new Payment();
 
-
     public static void main(String[] args) throws IOException {
         ArrayList<Member> memberList = new ArrayList<>();
         ArrayList<CompSwimmer> compList = new ArrayList<>();
         myMember.addToArray(memberList);
         mySwimmer.addToArrays(compList);
+
         //myStats.divisionSwimmers(compList);
         //myMember.addMember(memberList);
         //mySwimmer.addCompSwimmer(compList, memberList);
         //myFree.addFreeSwimmer(memberList);
         //myStats.divisionSwimmers(compList);
-        myPayments.showPayments();
+        //myPayments.showPayments();
+        //myMember.addMember(memberList);
+        //mySwimmer.addCompSwimmer(compList, memberList);
+
         Scanner scan = new Scanner(System.in);
 
-        /*int answer = scan.nextInt();
-        while (answer < 3) {
-            System.out.println("welcome to Svømme Delfin Klubben");
-            System.out.println("Do you want to add a member(press 1), change memberInformation(press 2), cancel member(press 3), see compostionmembers(press 4), " +
-                    " ");
 
+            System.out.println("*------------------------------------------------------------------------------------------*");
+            System.out.println("|                           Welcome to Svømme klubben Delfinen                             |");
+            System.out.println("|                   This program is used for easy overview of the members                  |");
+            System.out.println("|   You can the full memberlist, the mebers that has payed, and with members is in arreas. |");
+            System.out.println("|   For the trainers, there will be an easy access for the top swimmers in there division. |");
+            System.out.println("|                      And how they did in the previous tournaments                        |");
+            System.out.println("*------------------------------------------------------------------------------------------*");
+            System.out.println("                                  |                      |        ");
+            System.out.println("                                  |                      |        ");
+            System.out.println("                         *---------------------------------------*");
+            System.out.println("                         | Do you want to add a member (press 1) |");
+            System.out.println("                         | Change memberInformation    (press 2) |");
+            System.out.println("                         | Edit membership             (press 3) |");
+            System.out.println("                         | See competition members     (press 4) |");
+            System.out.println("                         *---------------------------------------*");
 
-        }
+            int answer = 0;
+            answer = scan.nextInt();
+            if (answer > 6) {
+                System.out.println(answer+" ERROR CODE 404");
+            }else {
+
 
 
         switch (answer) {
             case 1:
-                System.out.println("do you wish add a compSwimmer(press 1) freeswimmer(press 2?)");
+                System.out.println("*---------------------------------------*");
+                System.out.println("| Add a competition swimmer   (press 1) |");
+                System.out.println("| Add leisure swimmer         (press 2) |");
+                System.out.println("*---------------------------------------*");
                 answer = scan.nextInt();
                 switch (answer) {
                     case 1:
-                        mySwimmer.addCompSwimmer(compList, memberList);//laver et nyt objekt af typen Compswimmer, og kører metoden på objektet, og parameteroverfører de informationer som metoden skal indeholde.
+                       mySwimmer.addCompSwimmer(compList, memberList);//laver et nyt objekt af typen Compswimmer, og kører metoden på objektet, og parameteroverfører de informationer som metoden skal indeholde.
                         break;
                     case 2:
-                        myFree.addFreeSwimmer(memberList);//laver et nyt objekt af typen FreetupeSwimmer, etc.
+                        myFree.addMember(memberList);//laver et nyt objekt af typen FreetupeSwimmer, etc.
                         break;
                     default:
-                        System.out.print("try again");
+                        System.out.print("ERROR CODE 404");
                         break;
 
                 }
@@ -55,27 +76,18 @@ public class Main {
             case 3://cancelmember
                 break;
             case 4: //see positionSwimmers and pick.
-                System.out.print("Do you want to see CompositionsMembers in\n butterfly (press 1) \n crawl(press 2) \n rygcrawl(press 3) \n brystsvømning(press 4) ");
+                myStats.divisionSwimmers(compList);
                 answer = scan.nextInt();
                 break;
-            switch (answer) {
-                case 1: //butterfly - write to file i en sorteret Array
-                    break;
-                case 2: //crawl - write to file i en sorteret Array
-                    break;
-                case 3: //rygcrawl - write to file i en sorteret Array
-                    break;
-                case 4: // brystsvømning - write to file i en sorteret Array
-                    break;
-                default:
-                    System.out.print("try again");
-                    break;
 
-            }
 
             case 5:
                 //moneybalance
-                System.out.print("Do you want to see a full membershiplist(press 1) \n see a membershiplist for members who didnt pay(press 2) \n get total balancerapport(press 3)");
+                System.out.println("*------------------------------------*");
+                System.out.println("| See full membershiplist  (press 1) |");
+                System.out.println("| See members arreaslist   (press 2) |");
+                System.out.println("| Get total balancerapport (press 3) |");
+                System.out.println("*------------------------------------*");
                 answer = scan.nextInt();
                 switch (answer) {
                     case 1: // see full memberlist -
@@ -85,12 +97,14 @@ public class Main {
                     case 3://printing out balancerapport : expected total balanceamount for next year, last year:  amount of total balance for the members who payed, negativ total balanceamount (the members who didnt pay last year),
                         break;
                     default:
-                        System.out.println();
+                        System.out.println("ERROR CODE 404");
                         break;
                 }
-
-        }*/
-
+            default:
+                System.out.println("ERROR CODE 404");
+                break;
+            }
+        }
     }
 
 }
